8ada007d35b96b7adb53e860d4d42572eb28fca6
walasviana <140215357+walasviana@users.noreply.github.com>
2026-09-03 19:06:16 -0300

    Ativa autenticação do Bocha Scout

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import BochaScout from "./components/BochaScout";
+import AuthGate from "./components/AuthGate";
 import "./styles.css";
-ReactDOM.createRoot(document.getElementById("root")!).render(<React.StrictMode><BochaScout /></React.StrictMode>);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <AuthGate>
+      <BochaScout />
+    </AuthGate>
+  </React.StrictMode>
+);
