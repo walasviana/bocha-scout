7df7274956e19a2f9eda0462b10e1d3bbe35392d
walasviana <140215357+walasviana@users.noreply.github.com>
2026-09-03 22:59:54 -0300

    feat: add athlete quick start flow

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import BochaScout from "./components/BochaScout";
 import AuthGate from "./components/AuthGate";
+import AthleteQuickStart from "./components/AthleteQuickStart";
 import "./styles.css";
+
+function App() {
+  const [showScout, setShowScout] = useState(false);
+
+  return showScout
+    ? <BochaScout />
+    : <AthleteQuickStart onContinue={() => setShowScout(true)} />;
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthGate>
-      <BochaScout />
+      <App />
     </AuthGate>
   </React.StrictMode>
 );
