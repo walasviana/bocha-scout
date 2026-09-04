729e215b41c2ce3e4f965945d517cf81087bec83
walasviana <140215357+walasviana@users.noreply.github.com>
2026-09-03 23:47:11 -0300

    feat: abrir Bocha Scout direto com atletas disponíveis

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import BochaScout from "./components/BochaScout";
 import AuthGate from "./components/AuthGate";
-import AthleteQuickStart from "./components/AthleteQuickStart";
 import "./styles.css";
 
 function App() {
-  const [showScout, setShowScout] = useState(false);
-
-  return showScout
-    ? <BochaScout />
-    : <AthleteQuickStart onContinue={() => setShowScout(true)} />;
+  return <BochaScout />;
 }
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
