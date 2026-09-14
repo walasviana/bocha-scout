6cc56cedd97afe2c42eec9c17ed5f74e954d394f
walasviana <140215357+walasviana@users.noreply.github.com>
2026-09-13 22:54:06 -0300

    Enable automatic live throw timer

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import BochaScout from "./components/BochaScout";
 import AuthGate from "./components/AuthGate";
+import LiveTimerBridge from "./components/LiveTimerBridge";
 import "./styles.css";
 
 function App() {
-  return <BochaScout />;
+  return <><BochaScout /><LiveTimerBridge /></>;
 }
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
